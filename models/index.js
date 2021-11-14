1161d41be486acb681934b16b7740c2893d5d82d
lcko1012 <lecanhkieuoanh@gmail.com>
2021-11-14 22:52:11 +0700

    create-docker-file

--- models/index.js
+++ models/index.js
@@ -2,33 +2,33 @@ const dbConfig = require("../config/db.config")
 
 const Sequelize = require("sequelize")
 
-const sequelize = new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, {
-    host: dbConfig.HOST,
-    dialect: dbConfig.dialect,
-    operatorsAliases: false,
-
-    pool: {
-        max: dbConfig.pool.max,
-        min: dbConfig.pool.min,
-        acquire: dbConfig.pool.acquire,
-        idle: dbConfig.pool.idle
-    }
-})
-
-// Define server connection
-// const sequelize = new Sequelize(dbConfig.DB ,{
+// const sequelize = new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, {
+//     host: dbConfig.HOST,
 //     dialect: dbConfig.dialect,
-//     dialectOptions: dbConfig.dialectOptions,
+//     operatorsAliases: false,
+
+//     pool: {
+//         max: dbConfig.pool.max,
+//         min: dbConfig.pool.min,
+//         acquire: dbConfig.pool.acquire,
+//         idle: dbConfig.pool.idle
+//     }
 // })
 
-// sequelize
-//   .authenticate()
-//   .then(() => {
-//     console.log('Connection has been established successfully.');
-//   })
-//   .catch(err => {
-//     console.error('Unable to connect to the database:', err);
-// });
+// Define server connection
+const sequelize = new Sequelize(dbConfig.DB ,{
+    dialect: dbConfig.dialect,
+    dialectOptions: dbConfig.dialectOptions,
+})
+
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log('Connection has been established successfully.');
+  })
+  .catch(err => {
+    console.error('Unable to connect to the database:', err);
+});
 
 const db = {}
 
